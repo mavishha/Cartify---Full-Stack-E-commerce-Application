426233f71dd1320bca34c31a3eb824f09e079577
mavisha <vmavisha@gmail.com>
2026-03-28 00:50:23 +0530

    updated

--- backend-ecommerce/Ecommerce/src/main/java/com/example/demo/controller/StoreController.java
+++ backend-ecommerce/Ecommerce/src/main/java/com/example/demo/controller/StoreController.java
@@ -3,18 +3,23 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.Repository.StoreRepository;
 import com.example.demo.customexception.Resourcenotfound;
 import com.example.demo.model.Store;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 //data is passing
 @RestController
@@ -25,10 +30,14 @@ public class StoreController {
 	@Autowired 
 	private StoreRepository storerepo;
 	
+//	@GetMapping("/getstore")
+//	public List<Store> getStore(){
+//		return storerepo.findAll();
+//		
+//	}
 	@GetMapping("/getstore")
-	public List<Store> getStore(){
-		return storerepo.findAll();
-		
+	public List<Store> getStore(Pageable page){
+	    return storerepo.findAll(page).toList();
 	}
 	@PostMapping("/additem")
 	public Store addItems(@RequestBody Store item) {
@@ -45,6 +54,21 @@ public class StoreController {
 	public Store getOneitem(@PathVariable long id) {
 		return storerepo.findById(id)
 		.orElseThrow(()->new Resourcenotfound("this is not valid"+id));
+	}
+	@PutMapping("/edit/{id}")
+	public Store edit(@PathVariable long id, @RequestBody Store item) {
+		Store item1=storerepo.findById(id).orElseThrow(()->new Resourcenotfound("not valid"));
+		item1.setName(item.getName());
+		item1.setPrice(item.getPrice());
+		item1.setSeller(item.getSeller());
+		return storerepo.save(item1);
+	}
+	@DeleteMapping("/delete/{id}")
+	@ResponseStatus(value=HttpStatus.NO_CONTENT)
+	public String delete(@PathVariable long id) {
+		Store item=storerepo.findById(id).orElseThrow(()->new Resourcenotfound("not found"));
+		 storerepo.delete(item);
+		 return "successfully deleted with this id"+ id;
 	}
 	
 }
